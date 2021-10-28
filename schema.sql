d266c25d3541ef32cb1c8e78e9cea7890b758c1e
Amine Smahi <md.amine.smahi@gmail.com>
2021-10-28 11:00:14 +0100

    Create a table vets and 2 join tables specialization and visits

--- schema.sql
+++ schema.sql
@@ -38,4 +38,26 @@ ALTER TABLE animals ADD COLUMN owner_id INT,
   FOREIGN KEY (owner_id)
   REFERENCES owners (id);
 
-  
+CREATE TABLE vets(
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  age INT,
+  date DATE
+);
+
+CREATE TABLE specializations (
+  species_id INT,
+  vets_id INT,
+  PRIMARY KEY (species_id, vets_id),
+  CONSTRAINT fk_species FOREIGN KEY(species_id) REFERENCES species(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
+
+CREATE TABLE visits (
+  animals_id INT,
+  vets_id INT,
+  date DATE,
+  PRIMARY KEY (animals_id, vets_id),
+  CONSTRAINT animals_id FOREIGN KEY(animals_id) REFERENCES animals(id),
+  CONSTRAINT fk_vets FOREIGN KEY(vets_id) REFERENCES vets(id)
+);
