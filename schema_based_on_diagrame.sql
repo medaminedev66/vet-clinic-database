975234f0564ecb224bd08b7161b5e042b8a81da0
faizi2500 <faizan.zahid2500@gmail.com>
2021-11-02 17:33:42 +0500

    Add tables for invoice_items, treatments, and treatment_join_histories

--- schema_based_on_diagrame.sql
+++ schema_based_on_diagrame.sql
@@ -4,11 +4,11 @@ CREATE TABLE patients (
     date_of_birth
 );
 
-CREATE TABLE medical_hestories(
+CREATE TABLE medical_histories(
     id INT PRIMARY KEY,
     admitted_at TIMESTAMP,
     patient_id INT,
-    status VARCHAR,
+    status VARCHAR(35),
     CONSTRAINT fk_patient
       FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
@@ -19,8 +19,30 @@ CREATE TABLE invoices(
     generat_at TIMESTAMP,
     payed_at TIMESTAMP,
     medical_hestory_id INT,
-    CONSTRAINT fk_medical_hestorie
-    FOREIGN KEY (medical_hestory_id) REFERENCES medical_hestories(id)
+    CONSTRAINT fk_medical_history
+    FOREIGN KEY (medical_hestory_id) REFERENCES medical_histories(id)
 );
 
+CREATE TABLE invoice_items (
+    id INT PRIMARY KEY,
+    unit_price DECIMAL,
+    quantity INT,
+    total_price INT,
+    invoice_id INT,
+    treatment_id INT,
+    CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices(id),
+    CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
+);
 
+CREATE TABLE treatments (
+    id INT PRIMARY KEY,
+    type VARCHAR(35),
+    name VARCHAR(35)
+);
+
+CREATE TABLE treatments_join_histories (
+  treatment_id INT,
+  medical_history_id INT,
+  CONSTRAINT fk_tr FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
+  CONSTRAINT fk_mh FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE
+);
