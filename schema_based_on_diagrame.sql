1f3e12ad4db293ca2c8d8db07e94c15fd25e0ba0
Amine Smahi <md.amine.smahi@gmail.com>
2021-11-02 14:34:23 +0100

    Add indexes for the foreign keys

--- schema_based_on_diagrame.sql
+++ schema_based_on_diagrame.sql
@@ -1,7 +1,7 @@
 CREATE TABLE patients (
     id INT PRIMARY KEY,
     name TEXT,
-    date_of_birth
+    date_of_birth DATE
 );
 
 CREATE TABLE medical_histories(
@@ -12,6 +12,7 @@ CREATE TABLE medical_histories(
     CONSTRAINT fk_patient
       FOREIGN KEY(patient_id) REFERENCES patients(id)
 );
+CREATE INDEX patient_id_index ON medical_histories(patient_id);
 
 CREATE TABLE invoices(
     id INT PRIMARY KEY,
@@ -22,6 +23,7 @@ CREATE TABLE invoices(
     CONSTRAINT fk_medical_history
     FOREIGN KEY (medical_hestory_id) REFERENCES medical_histories(id)
 );
+CREATE INDEX med_id_index ON invoices(medical_hestory_id);
 
 CREATE TABLE invoice_items (
     id INT PRIMARY KEY,
@@ -33,6 +35,8 @@ CREATE TABLE invoice_items (
     CONSTRAINT fk_invoice FOREIGN KEY (invoice_id) REFERENCES invoices(id),
     CONSTRAINT fk_treatment FOREIGN KEY (treatment_id) REFERENCES treatments(id)
 );
+CREATE INDEX invoice_id_index ON invoice_items(invoice_id);
+CREATE INDEX treat_id_index ON invoice_items(treatment_id);
 
 CREATE TABLE treatments (
     id INT PRIMARY KEY,
@@ -46,3 +50,6 @@ CREATE TABLE treatments_join_histories (
   CONSTRAINT fk_tr FOREIGN KEY(treatment_id) REFERENCES treatments(id) ON DELETE CASCADE,
   CONSTRAINT fk_mh FOREIGN KEY(medical_history_id) REFERENCES medical_histories(id) ON DELETE CASCADE
 );
+
+CREATE INDEX treatment_id_index ON treatments_join_histories(treatment_id)
+CREATE INDEX medical_history_index ON treatments_join_histories(medical_history_id)
